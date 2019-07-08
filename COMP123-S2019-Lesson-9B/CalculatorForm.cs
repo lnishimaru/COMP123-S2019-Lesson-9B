d0fced3c3bfade8a1290180abf8c6095127c7569
Lilian Nishimaru De Souza <lilian.nishimaru@gmail.com>
2019-07-08 14:22:22 -0400

    Added Shared Event Handler for calculator buttons

--- COMP123-S2019-Lesson-9B/CalculatorForm.cs
+++ COMP123-S2019-Lesson-9B/CalculatorForm.cs
@@ -20,8 +20,16 @@ namespace COMP123_S2019_Lesson_9B
         private void ButtonEq_Click(object sender, EventArgs e)
         {
             var TheButton = sender as Button;
-            int result = int.Parse(TheButton.Text);
-            ResultLabel.Text = TheButton.Text;
+            int buttonValue;
+            bool resultCondition = int.TryParse(TheButton.Text,out buttonValue);
+            if (resultCondition)
+            {
+                ResultLabel.Text += TheButton.Text;
+            }
+            else
+            {
+                ResultLabel.Text = "Not a Number (NAN)";
+            }
         }
     }
 }
